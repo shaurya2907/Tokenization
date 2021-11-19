eb2689095c843f2c8da2931d20f9922f8fad42d4
shaurya <sharmashaurya2907@gmail.com>
2021-11-19 23:38:39 +0530

    commit

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -1,5 +1,6 @@
 const Token =artifacts.require("MyToken");
 
+
 var chai =require("chai");
 
 const BN = web3.utils.BN;
@@ -18,12 +19,17 @@ contract("Token Test", async (accounts)=>{
 
     const [deployerAccount, recipient, anotherAccount]=accounts;
 
+    beforeEach(async() => {
+this.myToken = await Token.new(process.env.INITIAL_TOKENS);
+
+    });
+
   it("all tokens should be in my account" , async()=>{
 
-let instance = await Token.deployed();
+let instance = this.myToken;
 let totalSupply = await instance.totalSupply();
 
- expect( await instance.balanceOf(deployerAccount)).to.be.a.bignumber.equal(totalSupply);
+ return expect( await instance.balanceOf(deployerAccount)).to.be.a.bignumber.equal(totalSupply);
 
     });
 
@@ -31,23 +37,23 @@ let totalSupply = await instance.totalSupply();
 
 it("is not possible to send more tokens than available in total", async()=>{
 
-    let instance = await Token.deployed();
+    let instance =  this.myToken;
     let balanceDeployer = await instance.balanceOf(deployerAccount);
 
     expect(instance.transfer(recipient,new BN(balanceDeployer+1))).to.eventually.be.rejected;
-    await expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceDeployer);
+   return  expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceDeployer);
  
 });
 
 it("is possible to send tokens between accounts", async()=>{
 
     const sendTokens=1;
-    let instance= await Token.deployed();
+    let instance= this.myToken;
     let totalSupply = await instance.totalSupply();
     expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
     expect(instance.transfer(recipient,sendTokens)).to.eventually.be.fulfilled;
 expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
-expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
+return await expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
 });
 
 });
---
+++ test/setupchai.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var chai =require("chai");
+
+const BN = web3.utils.BN;
+const chaiBN = require("chai-bn")(BN);
+
+chai.use(chaiBN);
+
+var chaiAsPromised = require("chai-as-promised");
+
+
+chai.use(chaiAsPromised);
+module.export= chai;
