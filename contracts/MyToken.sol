a9d7c15f6ccb63d379db54c5c588922747ac01da
shaurya <sharmashaurya2907@gmail.com>
2021-11-19 14:39:59 +0530

    commit

---
+++ contracts/MyToken.sol
@@ -0,0 +1,10 @@
+pragma solidity 0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Starducks Capuccino Token", "CAPPU") {
+        _mint(msg.sender, initialSupply);
+    
+    }
+}
