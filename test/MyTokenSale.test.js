9fc4e27908b45d923e3e4e7dd4ab50993c973ada
shaurya <sharmashaurya2907@gmail.com>
2021-11-20 01:49:31 +0530

    final tests pass

--- test/MyTokenSale.test.js
+++ test/MyTokenSale.test.js
@@ -14,8 +14,30 @@ contract("Token test", async(accounts)=>{
         let instance = await Token.deployed();
 expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(new BN(0));
 
-    })
+    });
 
+    it("all tokens should be in the TokenSale Smar COntract by default", async() =>{
+
+        let instance = await Token.deployed();
+        let balanceOfTokenSaleSmartContract = await instance.balanceOf(TokenSale.address);
+        let totalSupply= await instance.totalSupply();
+        expect(balanceOfTokenSaleSmartContract).to.be.a.bignumber.equal(totalSupply);
+
+    });
+
+    it("should be possile to buy tokens", async() =>{
+
+let tokenInstance = await Token.deployed();
+let tokenSaleinstance = await TokenSale.deployed();
+let balanceBefore = tokenInstance.balanceOf(deployerAccount);
+
+ await expect(tokenSaleinstance.sendTransaction({from:deployerAccount, value:web3.utils.toWei("1", "wei")})).to.be.fulfilled;
+expect(tokenInstance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceBefore+(new BN(1)));
+
+
+    });
+
+  
 
 
 
