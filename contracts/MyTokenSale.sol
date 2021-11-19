5ff80a0a0b927b4eea8eff9a01e4514804e71c46
shaurya <sharmashaurya2907@gmail.com>
2021-11-19 20:40:24 +0530

    Crowdsale

---
+++ contracts/MyTokenSale.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.0;
+
+import "./Crowdsale.sol";
+
+contract MyTokenSale is Crowdsale{
+
+   
+    constructor(
+        uint256 rate,    
+        address payable wallet,
+        IERC20 token
+    )
+        Crowdsale(rate, wallet, token)
+        public
+    {
+
+    }
+
+}
