16243776f8417411387e25a5f924ea58eedda497
shaurya <sharmashaurya2907@gmail.com>
2021-11-19 20:07:09 +0530

    new tests

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -16,11 +16,43 @@ const expect =chai.expect;
 
 contract("Token Test", async (accounts)=>{
 
+    const [deployerAccount, recipient, anotherAccount]=accounts;
+
+  it("all tokens should be in my account" , async()=>{
+
 let instance = await Token.deployed();
 let totalSupply = await instance.totalSupply();
-let balance = await instance.balanceOf(accounts[0]);
-expect(await instance.balanceOf(accounts[0])).to.be.a.bignumber.equal(totalSupply);
+
+ expect( await instance.balanceOf(deployerAccount)).to.be.a.bignumber.equal(totalSupply);
+
+    });
 
 
-})
+
+it("is not possible to send more tokens than available in total", async()=>{
+
+    let instance = await Token.deployed();
+    let balanceDeployer = await instance.balanceOf(deployerAccount);
+
+    expect(instance.transfer(recipient,new BN(balanceDeployer+1))).to.eventually.be.rejected;
+    await expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceDeployer);
+ 
+});
+
+it("is possible to send tokens between accounts", async()=>{
+
+    const sendTokens=1;
+    let instance= await Token.deployed();
+    let totalSupply = await instance.totalSupply();
+    expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+    expect(instance.transfer(recipient,sendTokens)).to.eventually.be.fulfilled;
+expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
+expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
+});
+
+});
+
+
+
+
 
