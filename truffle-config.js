2e9c5176187588140a636478cd9f8ecbd64d3c07
shaurya <sharmashaurya2907@gmail.com>
2021-11-21 00:25:50 +0530

    env

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const HDWalletProvider =rquire("@truffle/hdwallet-provide");
+const Mnemonic = "yard cost edge divide tourist eyebrow tomorrow invite kite rail grace choice ";
+const AccountIndex = 0;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -10,6 +13,12 @@ module.exports = {
     }
   },
 
+  gnache_local: {
+    provider: function(){
+      return new HDWalletProvider(Mnemonic, "http://127.0.0.1:7545", AccountIndex)
+    }
+  
+  },
   compilers: {
     solc:{
       version: "0.8.0"
