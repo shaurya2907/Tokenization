209e23d51182d72f27b5182a9a7aed909d4557c7
shaurya <sharmashaurya2907@gmail.com>
2021-11-22 00:34:58 +0530

    Updates

---
+++ precache-manifest.d2cbcbe17de1338ff21dd6debc05be74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f853494d6ef4f60fccdc90cc50e6d640",
+    "url": "/Tokenization/index.html"
+  },
+  {
+    "revision": "3d14842e1654193f3df1",
+    "url": "/Tokenization/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2b019f5e7003537b9dc3",
+    "url": "/Tokenization/static/js/2.372e5d54.chunk.js"
+  },
+  {
+    "revision": "3d14842e1654193f3df1",
+    "url": "/Tokenization/static/js/main.3c627223.chunk.js"
+  },
+  {
+    "revision": "2f869f7f52ed106b57e8",
+    "url": "/Tokenization/static/js/runtime-main.66578acf.js"
+  }
+]);
